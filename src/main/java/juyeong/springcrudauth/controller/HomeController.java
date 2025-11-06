431f22a9f85a342484e6684efdb7bd1c201d15dc
Juyeong Moon <whoor2828@gmail.com>
2025-11-06 20:19:37 +0900

    feat: 홈 메인 컨트롤러 추가 PostQueryService로 게시글 조회 후 model에 전달

---
+++ src/main/java/juyeong/springcrudauth/controller/HomeController.java
@@ -0,0 +1,28 @@
+package juyeong.springcrudauth.controller;
+
+import juyeong.springcrudauth.model.Post;
+import juyeong.springcrudauth.service.PostQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+public class homeController {
+    private final PostQueryService postQueryService;
+
+    @Autowired
+    public homeController(PostQueryService postQueryService) {
+        this.postQueryService = postQueryService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Post> posts = postQueryService.findPostPage();
+        model.addAttribute("posts", posts);
+        return "main";
+    }
+}
