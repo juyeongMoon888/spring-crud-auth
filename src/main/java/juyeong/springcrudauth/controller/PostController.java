329ae374928e2deecac0d71abe017cde69005d7a
Juyeong Moon <whoor2828@gmail.com>
2025-11-07 20:59:42 +0900

    feat: PostForm을 작성하는 @PostMapping 엔드포인트 /postWrite 추가

--- src/main/java/juyeong/springcrudauth/controller/PostController.java
+++ src/main/java/juyeong/springcrudauth/controller/PostController.java
@@ -3,15 +3,19 @@ package juyeong.springcrudauth.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostController {
 
     @GetMapping("/postForm")
-    public String writePostFrom() {
+    public String showPostFrom() {
         return "postForm";
     }
 
-    /*@PostMapping("/postWrite")
-    public*/
+    @PostMapping("/postWrite")
+    public String writePost(@RequestBody String title, String content) {
+        return "postWrite";
+    }
 }
