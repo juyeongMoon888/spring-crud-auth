eb277ba88ded0e418d8ec1d251f4409d56575040
Juyeong Moon <whoor2828@gmail.com>
2025-11-07 21:00:48 +0900

    style: 불필요한 import 제거

--- src/main/java/juyeong/springcrudauth/controller/HomeController.java
+++ src/main/java/juyeong/springcrudauth/controller/HomeController.java
@@ -6,16 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Controller
-public class homeController {
+public class HomeController {
     private final PostQueryService postQueryService;
 
     @Autowired
-    public homeController(PostQueryService postQueryService) {
+    public HomeController(PostQueryService postQueryService) {
         this.postQueryService = postQueryService;
     }
 
