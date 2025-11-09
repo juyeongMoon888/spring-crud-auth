985cfa43252dd3a9b8b6a22e01c91bc29c4fd2a9
juyeongMoon888 <whoor2828@gmail.com>
2025-11-09 14:15:13 +0900

    fix: updateById 삭제로 컨텍스트 초기화 오류 해결

--- src/main/java/juyeong/springcrudauth/repository/PostRepository.java
+++ src/main/java/juyeong/springcrudauth/repository/PostRepository.java
@@ -11,6 +11,4 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post, String> {
     Optional<Post> findById(int id);
-
-    int updateById(int id, PostEdit postEdit);
 }
